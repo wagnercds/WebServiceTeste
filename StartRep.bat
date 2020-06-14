b7b77f18a39872a41ea831b6df398efca27b01c9
wagnercds <wagnercds1978@gmail.com>
2020-06-14 13:45:56 -0300

    first commit

---
+++ StartRep.bat
@@ -0,0 +1,6 @@
+echo "# WebServiceTeste" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git remote add origin https://github.com/wagnercds/WebServiceTeste.git
+git push -u origin master
---
+++ WebServiceTeste/Service1.asmx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebServiceTeste
+{
+    /// <summary>
+    /// Summary description for Service1
+    /// </summary>
+    [WebService(Namespace = "http://com.tudoqueeinteressante/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class TesteService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public string Salvar(string param1, string param2)
+        {
+            return param1 + " " + param2;
+        }
+    }
+}
